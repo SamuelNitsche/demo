37fbe32ffb06cd2c81e3c0ed48a39ca08856cff5
Samuel Nitsche <mail@samynitsche.de>
2021-03-15 22:00:42 +0100

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
-			    <p>It work's 1</p>
+			    <p>It work's 2</p>
                         </div>
                     </div>
                 </div>
